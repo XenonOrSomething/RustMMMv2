9cc9aa2b201dd0597e295aa915696780ca86ec85
Xenon <143745702+XenonOrSomething@users.noreply.github.com>
2023-12-11 19:16:41 +0000

    you can load checkboxes dynamically now

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,76 @@
-use std::{path::PathBuf, fs::File, io::Write, string, process::Command};
-
-use egui::Context;
+use std::process::exit;
+use std::{path::PathBuf, fs::File, io::Write, string, process::Command, };
+use std::path;
+use downloader::Downloader;
+use egui::{Context, Checkbox, Widget};
 use rfd::FileDialog;
+use std::thread::spawn;
+use std::thread;
+use std::fs;
+use std::io::{prelude::*, Cursor};
+use zip_extract;
+//use egui_file::FileDialog;
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
+/// use downloader::Downloader;
+ // if we add new fields, give them default values when deserializing old state
+struct SimpleReporterPrivate {
+    last_update: std::time::Instant,
+    max_progress: Option<u64>,
+    message: String,
+}
+struct SimpleReporter {
+    private: std::sync::Mutex<Option<SimpleReporterPrivate>>,
+}
+
+impl SimpleReporter {
+    #[cfg(not(feature = "tui"))]
+    fn create() -> std::sync::Arc<Self> {
+        std::sync::Arc::new(Self {
+            private: std::sync::Mutex::new(None),
+        })
+    }
+}
+
+impl downloader::progress::Reporter for SimpleReporter {
+    fn setup(&self, max_progress: Option<u64>, message: &str) {
+        let private = SimpleReporterPrivate {
+            last_update: std::time::Instant::now(),
+            max_progress,
+            message: message.to_owned(),
+        };
+
+        let mut guard = self.private.lock().unwrap();
+        *guard = Some(private);
+    }
+
+    fn progress(&self, current: u64) {
+        if let Some(p) = self.private.lock().unwrap().as_mut() {
+            let max_bytes = match p.max_progress {
+                Some(bytes) => format!("{:?}", bytes),
+                None => "{unknown}".to_owned(),
+            };
+            if p.last_update.elapsed().as_millis() >= 1000 {
+                println!(
+                    "test file: {} of {} bytes. [{}]",
+                    current, max_bytes, p.message
+                );
+                p.last_update = std::time::Instant::now();
+            }
+        }
+    }
+
+    fn set_message(&self, message: &str) {
+        println!("test file: Message changed to: {}", message);
+    }
+
+    fn done(&self) {
+        let mut guard = self.private.lock().unwrap();
+        *guard = None;
+        println!("test file: [DONE]");
+    }
+}
 #[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
+#[serde(default)]
 pub struct TemplateApp {
     // Example stuff:
     label: String,
@@ -22,7 +88,14 @@ pub struct TemplateApp {
     isMMIv2: bool,
     isxenonMMI: bool,
     isCommunityMMI: bool,
-    
+    addedModsamt: i32,
+    checkboxes: Vec<bool>,
+    notFirstFrame: bool,
+    isUtilla: bool,
+    mods: Vec<String>,
+
+    //checkboxList: Vec<Checkbox>,
+    //opened_file: Option<PathBuf>,
 }
 
 impl Default for TemplateApp {
@@ -42,8 +115,13 @@ impl Default for TemplateApp {
             isMMIv2: false,
             isxenonMMI: false,
             isCommunityMMI: false,
-            
-
+            addedModsamt: 0,
+            checkboxes: vec![false, false],
+            notFirstFrame: false,
+            isUtilla: false,
+            mods: Vec::new(),
+           // checkboxList: Vec::new(),
+            //opened_file: Option::PathBuf::new(),
         }
     }
 }
@@ -76,7 +154,8 @@ impl eframe::App for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
-
+        
+        
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             
@@ -149,6 +228,8 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
+            
+            
             ui.horizontal(|ui| {
                 ui.heading("RustMMM");
                 
@@ -157,10 +238,69 @@ impl eframe::App for TemplateApp {
                 ui.text_edit_singleline(&mut self.label);
                 if(self.label != ""){
                     if(ui.button("✓").clicked()){
-                        println!("not implemented");
+                        fs::create_dir_all("C:\\RustMMMTemp");
+                        let mut downloader = Downloader::builder()
+                        .download_folder(std::path::Path::new("C:\\RustMMMTemp"))
+                        .parallel_requests(1)
+                        .build()
+                        .unwrap();
+
+                        let dl = downloader::Download::new("https://github.com/BepInEx/BepInEx/releases/download/v5.4.22/BepInEx_x64_5.4.22.0.zip");
+
+                        #[cfg(not(feature = "tui"))]
+                        let dl = dl.progress(SimpleReporter::create());
+
+                        
+
+            let result = downloader.download(&[dl]).unwrap();
+
+            for r in result {
+                match r {
+                    Err(e) => println!("Error: {}", e.to_string()),
+                    Ok(s) => println!("Success: {}", &s),
+                };
+            }
+            
+            if(self.isUtilla){
+                fs::create_dir_all("C:\\RustMMMTemp");
+                        let mut downloader = Downloader::builder()
+                        .download_folder(std::path::Path::new("C:\\RustMMMTemp"))
+                        .parallel_requests(1)
+                        .build()
+                        .unwrap();
+
+                        let dl = downloader::Download::new("https://github.com/legoandmars/Utilla/releases/download/v1.6.10/Utilla.zip");
+
+                        #[cfg(not(feature = "tui"))]
+                        let dl = dl.progress(SimpleReporter::create());
+
+                        
+
+            let result = downloader.download(&[dl]).unwrap();
+
+            for r in result {
+
+                match r {
+                    Err(e) => println!("Error: {}", e.to_string()),
+                    Ok(s) => println!("Success: {}", &s),
+                };
+            }
+            let bytes = "C:\\RustMMMTemp\\BepInEx_x64_5.4.22.0.zip".as_bytes().to_vec();
+            let archive: Vec<u8> = bytes;
+            let target_dir = PathBuf::from(&self.label);
+            let ex1 = zip_extract::extract(Cursor::new(archive), &target_dir, true);
+            println!("extracted");
+            let bytes = "C:\\RustMMMTemp\\Utilla.zip".as_bytes().to_vec();
+            let archive: Vec<u8> = bytes;
+            let target_dir = PathBuf::from(&self.label);
+            let extractedzip2 = zip_extract::extract(Cursor::new(archive), &target_dir, true);
+            println!("extracted");
+            }
                     }
                 }else{
                     if(ui.button("...").clicked()){
+                            
+                        
                         let files = FileDialog::new()
                         .add_filter("Gorilla Tag", &["exe"])
                         
@@ -178,11 +318,14 @@ impl eframe::App for TemplateApp {
                         }
                         
                         
+                        
+                        
                     }
                 }
                 if(self.isErrorShown){
                     egui::Window::new("Help").show(ctx, |ui| {
-                        
+                        ui.label("Help not implemented yet. dosent pull from any repos yet");
+                        /* 
                         egui::ScrollArea::vertical().show(ui, |ui| {
                             ui.heading("Selecting game folder");
                             
@@ -206,7 +349,9 @@ impl eframe::App for TemplateApp {
                             ui.add_space(16.0);
                             ui.label("- TODO: PLACE AN IMGE HERE")
                             
+                            
                         });
+                        */
                         ui.separator();
                         if(ui.button("Ok").clicked()){
                             self.isErrorShown = false;
@@ -233,11 +378,47 @@ impl eframe::App for TemplateApp {
 
             
 
+            ui.separator();
+            if(!self.notFirstFrame){
+                self.notFirstFrame = true;
+                self.mods.append(&mut vec!["BepInEx".to_string()]);
+                self.mods.append(&mut vec!["Utilla".to_string()]);
+                self.mods.append(&mut vec!["Bark".to_string()]);
+                
+                
+            } 
+            
+                println!("{:?}", self.mods);
+                println!("checkboxes");
+                for (i, el) in self.mods.iter().enumerate() {
+                    self.addedModsamt += 1;
+                    println!("Mods in database: {}", el);
+                    println!("The current index is {}", i);
+                    
+        
+                 
+                    self.checkboxes.append(&mut vec![false]);
+                    ui.checkbox(&mut self.checkboxes[i], el);
+                    println!("added");
+                    
+                
+            }
+            //checkboxes(ui);
+            
+                
+                    
+                    
+                    
+                
+            
+            
+       
+            
             
 
             ui.separator();
 
-            
+            self.checkboxes.append(&mut vec![true]);
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 
@@ -246,21 +427,31 @@ impl eframe::App for TemplateApp {
             });
         });
     }
+
+    
+
+    fn save(&mut self, _storage: &mut dyn eframe::Storage) {}
+
+    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {}
+
+    fn auto_save_interval(&self) -> std::time::Duration {
+        std::time::Duration::from_secs(30)
+    }
+
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        // NOTE: a bright gray makes the shadows of the windows look weird.
+        // We use a bit of transparency so that if the user switches on the
+        // `transparent()` option they get immediate results.
+        egui::Color32::from_rgba_unmultiplied(12, 12, 12, 180).to_normalized_gamma_f32()
+
+        // _visuals.window_fill() would also be a natural choice
+    }
+
+    fn persist_egui_memory(&self) -> bool {
+        true
+    }
 }
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
 
 fn saveDir(path: &[u8]) -> std::io::Result<()> {
     
@@ -269,5 +460,6 @@ fn saveDir(path: &[u8]) -> std::io::Result<()> {
     file.write_all(path)?;
     Ok(())
 }
+
 
 
